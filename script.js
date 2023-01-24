9a5e47ef2b3b5ff15873f0b7f0aa55265e9269a2
renanvicentim <renanvicentim97@gmail.com>
2023-01-24 19:57:04 -0300

    Projeto Playstation Store

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuList = document.getElementById("menu-list")
+
+menuList.style.maxHeight = "0px"
+
+function showMenu() {
+    if(menuList.style.maxHeight == "0px"){
+        menuList.style.maxHeight = "120px"
+    } else {
+        menuList.style.maxHeight = "0px"
+    }
+}
